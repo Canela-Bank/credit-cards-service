c18e51b88f3447222dcac448e44d0e9cb17c8edd
Carlos Loreto <sa_carlos@javeriana.edu.co>
2022-11-01 16:54:25 -0500

    Update CreditCardRequestController.java

--- src/main/java/com/canela/service/creditcardmgmt/rest/CreditCardRequestController.java
+++ src/main/java/com/canela/service/creditcardmgmt/rest/CreditCardRequestController.java
@@ -2,49 +2,41 @@ package com.canela.service.creditcardmgmt.rest;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.canela.service.creditcardmgmt.exceptions.CreditCardRequestResponseException;
-
-import io.netty.handler.codec.json.JsonObjectDecoder;
-
-
 
 @RestController
 @RequestMapping("api/creditcardmgmt")
 public class CreditCardRequestController {
 	
-	@GetMapping("requestCreditCard")
+	  StringBuilder result = null;
+	
+	@PostMapping("requestCreditCard")
 	public String creditCardRequest(@RequestBody String client) {
 		
-
-		  StringBuilder result = null;
 	    
-		try {
-			
-		  
-			
+		try { 
+			// Add URL API 
 			URL url = new URL ("http://localhost:8081/api/centralderiesgo/getReports");
-
+			
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			
             con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             con.setRequestProperty("Accept", "application/json");
             con.setDoOutput(true);
             con.setDoInput(true);
             con.setRequestMethod("POST");
-
+            
             OutputStream os = con.getOutputStream();
             os.write(client.getBytes("UTF-8"));
             os.close();         
@@ -66,8 +58,7 @@ public class CreditCardRequestController {
                        while ((line = reader.readLine()) != null) {
                            result.append(line);
                           
-                       }
-                       
+                       }       
                        
                        System.out.println("result: " + result.toString()) ;
                        
@@ -83,22 +74,15 @@ public class CreditCardRequestController {
                        
                    } catch (Exception e) {
                        e.printStackTrace();
-                   }
-            	   
+                   }       	   
             	   	                                 
                }
                		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-		return "Request Sent...";      
-		
+			
+		return "Request Sent...";      	
 	}
-	
-
-	
-	
 	
 }
